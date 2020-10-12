224bf67e19882ea4c27879b531f1c24c0601c0fb
Lin Jiongjiong <lefortyop@gmail.com>
2020-10-12 18:01:08 +0200

    add scripts meshes etc

---
+++ Assets/Scripts/GameManagers/OddRange_Attribut.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace GameManagers
+{
+    public class OddRangeAttribute : PropertyAttribute
+    {
+
+        public int Min { get; }
+        public int Max { get; }
+
+
+        public OddRangeAttribute(int min, int max)
+        {
+            Min = min;
+            Max = max;
+        }
+    }
+}
---
+++ Assets/Scripts/Map/Map_Scripts.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GameManagers;
+using UnityEngine;
+
+
+
+
+namespace Bomberboy.Map
+{
+    public class Map_Scripts : MonoBehaviour
+    {
+        
+        [SerializeField] [OddRange(3,30)] private int _width;
+        public int width => _width;
+
+        [SerializeField] [OddRange(3, 30)] private int _height;
+        public int height => _height;
+        
+        
+        
+        
+        
+        
+    }
+}
